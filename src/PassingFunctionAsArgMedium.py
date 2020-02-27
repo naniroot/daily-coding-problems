d106253403a825b5033db45c541793936800a48f
nravikumar <nravikumar@twilio.com>
2020-02-26 19:06:54 -0800

    passing function arg

---
+++ src/PassingFunctionAsArgMedium.py
@@ -0,0 +1,30 @@
+'''
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.
+ For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
+'''
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+
+def car(a, b):
+    return a
+
+
+def cdr(a, b):
+    return b
+
+
+k = cons(3, 4)
+print(k(car))
+print(k(cdr))
